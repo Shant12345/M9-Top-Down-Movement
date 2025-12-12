9b2716d53a12ba4e9ed2fe2016ca4d16dd5eb2b2
Shant <shdorsey@s.sfusd.edu>
2025-12-12 11:21:10 -0800

    M9L10

--- lessons/game.gd
+++ lessons/game.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
+@onready var runner: Runner = %Runner
+@onready var count_down: CountDown = %CountDown
 @onready var finish_line: FinishLine = $FinishLine
 
 func _ready() -> void:
+	count_down.start_counting()
+	runner.set_physics_process(false)
+	count_down.counting_finished.connect(
+		func() -> void:
+			runner.set_physics_process(true)
+	)
+	
 	finish_line.body_entered.connect(func (body: Node) -> void:
 		if body is not Runner:
 			return
+		@warning_ignore("shadowed_variable")
 		var runner := body as Runner
 
 		runner.set_physics_process(false)
